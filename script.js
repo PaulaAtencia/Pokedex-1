3fbe36699c5e8d220537231396e99d60021cfac8
Paula <patebar593@g.educaand.es>
2024-02-24 18:13:38 +0100

    Cambios

--- script.js
+++ script.js
@@ -4,11 +4,9 @@
 const datosPokemon = [];
 
 // Función para cargar la lista de Pokémon
-async function cargarTodosPokemon() 
-{
+async function cargarTodosPokemon() {
     // Cargar y guardar los datos de los 151 Pokémon
-    for (var i = 1; i <= 151; i++) 
-    {
+    for (let i = 1; i <= 151; i++) {
         datosPokemon.push(await getPokemon(i));
     }
 }
@@ -18,16 +16,16 @@ async function cargarTodosPokemon()
 document.addEventListener(`DOMContentLoaded`, async function () {
 
     await cargarTodosPokemon();
-    mostrarPokemonList(datosPokemon);
+    await mostrarPokemonList(datosPokemon);
 
 });
 
 const buscador = document.getElementById(`buscador`);
 // Función que se hace al usar el buscador, hace que solo aparezcan los pokémon que contengan el texto indicado
-buscador.addEventListener(`input`, async function(event) {
+buscador.addEventListener(`input`, async function (event) {
 
     const busqueda = event.target.value.trim();
-    
+
     await filtrarListaPorNombre(busqueda);
 
     verificarModo();
@@ -35,62 +33,58 @@ buscador.addEventListener(`input`, async function(event) {
 
 
 // Función que filtra los pokémon que empiezan por el string dado
-async function filtrarListaPorNombre(busqueda)
-{
-    var listaFiltrada = datosPokemon.filter( function(pokemon) {
+async function filtrarListaPorNombre(busqueda) {
+    let listaFiltrada = datosPokemon.filter(function (pokemon) {
         return pokemon.forms[0].name.toLowerCase().includes(busqueda.toLowerCase());
     });
 
     mostrarPokemonList(listaFiltrada);
 
     // Si no hay resultados el borde del buscador es rojo
-    if (busqueda.length > 0 && listaFiltrada.length == 0) 
-    {
+    if (busqueda.length > 0 && listaFiltrada.length == 0) {
         buscador.classList.remove(`conResultados`);
         buscador.classList.add(`sinResultados`);
-    } 
+    }
     // Si hay resultados el borde del buscador es verde
-    else if (busqueda.length > 0 && listaFiltrada.length > 0)
-    {
+    else if (busqueda.length > 0 && listaFiltrada.length > 0) {
         buscador.classList.remove(`sinResultados`);
         buscador.classList.add(`conResultados`);
     }
     // Si no hay texto en el buscador se queda azul
-    else 
-    {
+    else {
         buscador.classList.remove(`conResultados`);
         buscador.classList.remove(`sinResultados`);
     }
 
 }
 
+// Lista de pokemons
+const lista = document.getElementById(`lista-pokemons`);
+
 
 // Crea la lista con los pokémon que se les pasa por datos
 async function mostrarPokemonList(datos) {
-
-    // Obtener la referencia del elemento ul
-    var lista = document.getElementById(`lista-pokemons`);
 
     // Limpiar la lista antes de agregar nuevos elementos
     lista.innerHTML = ``;
 
     // Iterar sobre los datos y generar elementos 
-    for (var i = 0; i < datos.length; i++) {
+    for (let i = 0; i < datos.length; i++) {
 
         // Calculamos el string adaptado a 3 cifras del número del pokémon
-        var numeroPokemon = String(datos[i].id).padStart(3, `0`);
+        let numeroPokemon = String(datos[i].id).padStart(3, `0`);
 
         // Crear un nuevo elemento
-        var nuevoElemento = document.createElement(`div`);
+        let nuevoElemento = document.createElement(`div`);
         // Configurar el contenido, estilo y clase del nuevo elemento
-        nuevoElemento.className = `contenedor-elemento`;
+        nuevoElemento.className = `contenedor-elemento elemento-animado`;
 
 
         // Creamos el enlace clickeable de la imagen
-        var urlImagen = document.createElement(`a`);
+        let urlImagen = document.createElement(`a`);
         urlImagen.href = ``;
         // Creamos la imagen del pokémon
-        var imagen = document.createElement(`img`);
+        let imagen = document.createElement(`img`);
         imagen.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${numeroPokemon}.png`;
         // La hacemos clickeable
         urlImagen.appendChild(imagen);
@@ -99,7 +93,7 @@ async function mostrarPokemonList(datos) {
 
 
         // Creamos el contenedor del nombre
-        var divNumero = document.createElement(`div`);
+        let divNumero = document.createElement(`div`);
         divNumero.className = `contenedor-numero`;
         divNumero.textContent = `Nº ` + numeroPokemon;
         // Añadir contenedor del nombre al elemento
@@ -107,10 +101,10 @@ async function mostrarPokemonList(datos) {
 
 
         // Obtener nombre y poner primera letra en mayúscula
-        var nombrePokemon = datos[i].name;
+        let nombrePokemon = datos[i].name;
         nombrePokemon = nombrePokemon.charAt(0).toUpperCase() + nombrePokemon.slice(1);
         // Creamos el contenedor del nombre
-        var divNombre = document.createElement(`div`);
+        let divNombre = document.createElement(`div`);
         divNombre.className = `contenedor-nombre`;
         divNombre.textContent = nombrePokemon;
         // Añadir contenedor del nombre al elemento
@@ -118,13 +112,13 @@ async function mostrarPokemonList(datos) {
 
 
         // Creamos el contenedor de los tipos
-        var divTipos = document.createElement(`div`);
+        let divTipos = document.createElement(`div`);
         divTipos.className = `contenedor-tipos`;
 
         // Creamos el contenedor del primer tipo
-        var divTipo1 = document.createElement(`div`);
+        let divTipo1 = document.createElement(`div`);
         // Creamos el contenedor del texto
-        var textoTipo1 = document.createElement(`div`);
+        let textoTipo1 = document.createElement(`div`);
         textoTipo1.className = `contenedor-texto-tipo`;
         // Traducimos el primer tipo y lo añadimos al contenedor
         textoTipo1.textContent = await traducirTipo(datos[i].types[0].type.name);
@@ -136,14 +130,17 @@ async function mostrarPokemonList(datos) {
         divTipo1.width = textoTipo1.width * 3;
         // Añadir contenedor de tipo 1 a Tipos
         divTipos.appendChild(divTipo1);
+        // Añadir tipo 1 a las clase del elemento
+        nuevoElemento.classList.add(textoTipo1.textContent + `t1`);
+        // Añadimos tipo 2 a las clase del elemento como si fuera nulo (por si no tiene)
+        nuevoElemento.classList.add(`nullt2`);
 
         // Añadir si tiene segundo tipo
-        if (datos[i].types[1] != null) 
-        {
+        if (datos[i].types[1] != null) {
             // Creamos el contenedor del segundo tipo
-            var divTipo2 = document.createElement(`div`);
+            let divTipo2 = document.createElement(`div`);
             // Creamos el contenedor del texto
-            var textoTipo2 = document.createElement(`div`);
+            let textoTipo2 = document.createElement(`div`);
             textoTipo2.className = `contenedor-texto-tipo`;
             // Traducimos el segundo tipo y lo añadimos al contenedor
             textoTipo2.textContent = await traducirTipo(datos[i].types[1].type.name);
@@ -155,6 +152,8 @@ async function mostrarPokemonList(datos) {
             divTipo2.width = textoTipo2.width * 3;
             // Añadir contenedor de tipo 2 a Tipos
             divTipos.appendChild(divTipo2);
+            // Añadir tipo 2 a las clase del elemento
+            nuevoElemento.classList.add(textoTipo2.textContent + `t2`);
         }
 
         // Añadir contenedor de los tipos al elemento
@@ -164,9 +163,8 @@ async function mostrarPokemonList(datos) {
         lista.appendChild(nuevoElemento);
     }
 
-    if(datos.length === 0)
-    {
-        var mensajeError = document.createElement(`div`);
+    if (datos.length === 0) {
+        let mensajeError = document.createElement(`div`);
         mensajeError.className = `mensaje-error`;
         mensajeError.textContent = `No se han encontrado coincidencias, por favor revise su búsqueda.`;
         lista.appendChild(mensajeError);
@@ -174,6 +172,13 @@ async function mostrarPokemonList(datos) {
 
     verificarModo(); // Revisa el modo claro/oscuro para la lista
     controlFlechas(); // Revisa la aparición de las flechas
+    elementosAnimados = document.querySelectorAll(`.elemento-animado`); // Actualizar lista para las animaciones
+
+    // Establecer animaciones a los elementos de la lista
+    elementosAnimados.forEach(elemento => {
+        elemento.addEventListener(`mouseover`, animarFondoTarjeta);
+        elemento.addEventListener(`mouseout`, restablecerFondoTarjeta);
+    });
 }
 
 // Obtiene los datos de un pokemon pasando su id o su nombre
@@ -242,7 +247,7 @@ async function traducirTipo(tipo) {
 
 document.addEventListener(`DOMContentLoaded`, async function () {
 
-    var boton = document.getElementById(`imagen-modo`);
+    let boton = document.getElementById(`imagen-modo`);
 
     boton.addEventListener(`click`, cambiarModo);
 
@@ -250,22 +255,14 @@ document.addEventListener(`DOMContentLoaded`, async function () {
     verificarModo();
 });
 
-// Función para simular un tiempo de espera
-function esperar(ms) 
-{
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function cambiarModo() 
-{
-    var body = document.body;
-    var pokemons = Array.from(document.getElementsByClassName(`contenedor-elemento`));
+function cambiarModo() {
+    let body = document.body;
+    let pokemons = Array.from(document.getElementsByClassName(`contenedor-elemento`));
 
     // Obtener el estado actual del modo
-    var modoActual = body.classList.contains(`modo-oscuro`);
+    let modoActual = body.classList.contains(`modo-oscuro`);
 
-    if (modoActual) 
-    {
+    if (modoActual) {
         body.classList.remove(`modo-oscuro`);
         body.classList.add(`modo-claro`);
 
@@ -278,9 +275,8 @@ function cambiarModo()
             pokemon.querySelector(`:nth-child(3)`).classList.remove(`modo-oscuro`);
         });
 
-    } 
-    else 
-    {
+    }
+    else {
         body.classList.remove(`modo-claro`);
         body.classList.add(`modo-oscuro`);
 
@@ -301,23 +297,20 @@ function cambiarModo()
     localStorage.setItem(`modo-oscuro-activado`, modoActualizado);
 }
 
-function verificarModo() 
-{
-    var body = document.body;
-    var pokemons = Array.from(document.getElementsByClassName(`contenedor-elemento`));
+function verificarModo() {
+    let body = document.body;
+    let pokemons = Array.from(document.getElementsByClassName(`contenedor-elemento`));
 
     // Verificar si el modo oscuro está activo en localStorage
-    var modoGuardado = localStorage.getItem(`modo-oscuro-activado`);
+    let modoGuardado = localStorage.getItem(`modo-oscuro-activado`);
 
     // Si es la primera vez
-    if(modoGuardado === null)
-    {
+    if (modoGuardado === null) {
         localStorage.setItem(`modo-oscuro-activado`, false);
         modoGuardado = localStorage.getItem(`modo-oscuro-activado`);
     }
 
-    if (modoGuardado === `true`) 
-    {
+    if (modoGuardado === `true`) {
         body.classList.add(`modo-oscuro`);
 
         pokemons.forEach(pokemon => {
@@ -325,9 +318,8 @@ function verificarModo()
             pokemon.querySelector(`:nth-child(2)`).classList.add(`modo-oscuro`);
             pokemon.querySelector(`:nth-child(3)`).classList.add(`modo-oscuro`);
         });
-    } 
-    else 
-    {
+    }
+    else {
         body.classList.add(`modo-claro`);
 
         pokemons.forEach(pokemon => {
@@ -344,40 +336,35 @@ function verificarModo()
 // Imagen que te envía al fondo de la lista
 const flechaAbajo = document.getElementById(`contenedor-flecha-abajo`);
 // Función que realiza la acción al clickear la imagen
-flechaAbajo.addEventListener(`click`, function() {
-    window.scrollTo({top: document.body.scrollHeight, behavior: `smooth`});
+flechaAbajo.addEventListener(`click`, function () {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: `smooth` });
 });
 
 // Imagen que te envía al inicio de la lista
 const flechaArriba = document.getElementById(`contenedor-flecha-arriba`);
 // Función que realiza la acción al clickear la imagen
-flechaArriba.addEventListener(`click`, function() {
-    window.scrollTo({top: 0, behavior: `smooth`});
+flechaArriba.addEventListener(`click`, function () {
+    window.scrollTo({ top: 0, behavior: `smooth` });
 });
 
 // Función para mostrar/ocultar las flechas según la posición donde estás en la página
-function controlFlechas() 
-{
+function controlFlechas() {
     //La flecha que desciende hasta abajo no sale si estás en la parte más baja
-    if (window.scrollY < (document.body.offsetHeight - window.innerHeight)) 
-    {
+    if (window.scrollY < (document.body.offsetHeight - window.innerHeight)) {
         // Se muestra
         flechaAbajo.style.display = `block`;
-    } 
-    else 
-    {
+    }
+    else {
         // Se oculta
         flechaAbajo.style.display = `none`;
     }
 
     // La flecha que sube hasta arriba no sale si estas en la parte más alta
-    if (window.scrollY > 8) 
-    {
+    if (window.scrollY > 8) {
         // Se muestra
         flechaArriba.style.display = `block`;
-    } 
-    else 
-    {
+    }
+    else {
         // Se oculta
         flechaArriba.style.display = `none`;
     }
@@ -385,3 +372,31 @@ function controlFlechas()
 
 // La acción de scroll activa la función
 window.addEventListener(`scroll`, controlFlechas);
+
+/***********************************************************************************************************/
+/* GESTIÓN DE LAS ANIMACIONES DE LAS TARJETAS */
+
+// Establecer los colores de fondo de la tarjetas con los de su tipo
+function animarFondoTarjeta(event) {
+
+    // Pokemon seleccionado
+    const tarjetaPokemon = event.currentTarget;
+    // Contenedor de sus tipos
+    const tipos = tarjetaPokemon.querySelector('.contenedor-tipos');
+    // Color del tipo 1
+    const color1 = window.getComputedStyle(tipos.querySelector(':nth-child(1)')).backgroundColor;
+    // Color del tipo 2
+    const color2 = tipos.querySelector(':nth-child(2)') ? window.getComputedStyle(tipos.querySelector(':nth-child(2)')).backgroundColor : null;
+    // Establecer los colores de fondo de la tarjeta
+    tarjetaPokemon.style.background = `linear-gradient(to left bottom, ${color1} 50%, ${color2 ? color2 : color1} 50%)`;
+
+}
+
+// Restablecer el color de la tarjeta
+function restablecerFondoTarjeta(event) {
+    const tarjetaPokemon = event.currentTarget;
+    tarjetaPokemon.style.background = ``;
+}
+
+// Elementos animados de la lista
+let elementosAnimados;
